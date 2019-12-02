717da4443efc9bcb5d48da5ee1f74c4445e7dcae
Im-Tae <dlaxo2420@naver.com>
2019-12-02 21:14:23 +0900

    heeeeellllllllpppp

--- Android/Memorandum/app/src/main/java/com/memorandum/MainActivity.kt
+++ Android/Memorandum/app/src/main/java/com/memorandum/MainActivity.kt
@@ -2,11 +2,14 @@ package com.memorandum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ Android/Memorandum/app/src/main/java/com/memorandum/splashscreen/SplashActivity.kt
@@ -0,0 +1,40 @@
+package com.memorandum.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import com.memorandum.R
+import com.memorandum.User.LoginActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        var textAnimation = AnimationUtils.loadAnimation(this, R.anim.text)
+        var layoutAnimation = AnimationUtils.loadAnimation(this, R.anim.background)
+
+        val splashScreen = object : Thread() {
+            override fun run() {
+                titleText.startAnimation(textAnimation)
+                headingText.startAnimation(textAnimation)
+                splashlayout.startAnimation(layoutAnimation)
+
+                sleep(4000)
+                startActivity<LoginActivity>()
+                finish()
+            }
+        }
+        splashScreen.start()
+
+
+
+
+
+    }
+}
